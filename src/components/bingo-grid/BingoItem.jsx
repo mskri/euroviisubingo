95653b2870f3a6717b9c0a83e41e2cce51e3cd42
Sakari Mursu <sakari@mursu.dev>
2019-05-17 00:02:52 +0300

    Support break hints in item texts

--- src/components/bingo-grid/BingoItem.jsx
+++ src/components/bingo-grid/BingoItem.jsx
@@ -1,22 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styles from './BingoItem.module.css';
+import styles from "./BingoItem.module.css";
 
 const BingoItem = props => {
-    const { text } = props;
-    const [active, setActive] = useState(false);
+  const { text } = props;
+  const [active, setActive] = useState(false);
 
-    const handleClick = () => {
-        setActive(!active);
-    };
+  const handleClick = () => {
+    setActive(!active);
+  };
 
-    return (
-        <div className={styles.container} onClick={handleClick}>
-            <div className={styles.wrapper}>
-                <div className={styles.text}>{text}</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container} onClick={handleClick}>
+      <div className={styles.wrapper}>
+        <div
+          className={styles.text}
+          dangerouslySetInnerHTML={{ __html: text }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BingoItem;
